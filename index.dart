bc30ed599fa5e38f7baeaefbc196b2a78a380b49
JoaquinAdan <rodriguezjdev@gmail.com>
2023-04-27 11:13:39 -0300

    first commit

---
+++ index.dart
@@ -0,0 +1,20 @@
+// Todo archivo de flutter va dentro de un void main()
+void mainBool() {
+  print('Programa para saber si esta soleado');
+  bool estaSoleado = true;
+  print(estaSoleado);
+}
+
+void mainNumber() {
+  print('Programa para saber la edad y estatura');
+  int edad = 20;
+  print('Edad $edad');
+  double estatura = 1.65;
+  print('Estatura $estatura');
+}
+
+void mainString() {
+  print('Programa para saber el nombre de un pais');
+  String pais = 'Argentina';
+  print('Nombre del pais $pais');
+}
